63e0bd8ff1db978c1ac6a066562d3d511f76d06f
Yoann04 <yoann3921monnin@hotmail.fr>
2025-04-13 00:21:09 +0200

    Connexion API

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -111,7 +111,7 @@ let dataForm = new FormData(formInscription);
   redirect: "follow"
 };
 
-fetch("http://127.0.0.1:8000/api/registration", requestOptions)
+fetch(apiUrl+"registration", requestOptions)
   .then(response => {
     if(response.ok){
     return response.json();
